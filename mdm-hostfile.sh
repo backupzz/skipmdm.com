55ac9d924d6cdfb5b5e19f9ee0fc7f8fae3560cf
2pravin7 <2pravin7@gmail.com>
2024-03-19 14:19:32 -0700

    .

--- mdm-hostfile.sh
+++ mdm-hostfile.sh
@@ -4,10 +4,12 @@
 
 echo -e "Blocking MDM hosts..."
 
+grep -qxF '0.0.0.0 mdmenrollment.apple.com' /etc/hosts || echo "0.0.0.0 abc.def.ghi" >> /etc/hosts
 grep -qxF '0.0.0.0 deviceenrollment.apple.com' /etc/hosts || echo "0.0.0.0 deviceenrollment.apple.com" >> /etc/hosts
 grep -qxF '0.0.0.0 gdmf.apple.com' /etc/hosts || echo "0.0.0.0 gdmf.apple.com" >> /etc/hosts
 grep -qxF '0.0.0.0 iprofiles.apple.com' /etc/hosts || echo "0.0.0.0 iprofiles.apple.com" >> /etc/hosts
 grep -qxF '0.0.0.0 mdmenrollment.apple.com' /etc/hosts || echo "0.0.0.0 mdmenrollment.apple.com" >> /etc/hosts
+
 
 echo -e "Successfully blocked hosts\n"
 
